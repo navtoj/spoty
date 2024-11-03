b5847f3987ee747ea2c838a7720d11474976719c
Navtoj Chahal <github@navtoj.com>
2024-11-02 23:09:20 -0700

    Follow System Theme
    w/ `mode-watcher`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import { ModeWatcher } from 'mode-watcher';
 </script>
+
+<ModeWatcher themeColors={{ dark: 'black', light: 'white' }} />
 
 {@render children()}
